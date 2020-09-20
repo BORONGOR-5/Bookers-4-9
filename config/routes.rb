5052f701cdb011e14f1d87d885d135a254b65198
EC2 Default User <ec2-user@ip-172-31-3-81.ec2.internal>
2020-09-20 19:29:18 +0000

    途中１-homeの記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   devise_for :books
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :books
+  resources :users
+
+# Rails.application.routes.draw do
+  root 'home#index'
+# end
 end
