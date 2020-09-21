f0bdff80bc5b71275fb0dd2e262355fda004f25e
EC2 Default User <ec2-user@ip-172-31-3-81.ec2.internal>
2020-09-21 12:06:27 +0000

    途中５-login-signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,25 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    users_path # ログイン後に遷移するpathを設定
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path # ログアウト後に遷移するpathを設定
+  end
+
+
+
+
+
+
+
+
+
+
   protected
 
   def configure_permitted_parameters
