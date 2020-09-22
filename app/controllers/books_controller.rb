029ea24f3b57459598de1ae8b27c21b33419cf87
EC2 Default User <ec2-user@ip-172-31-3-81.ec2.internal>
2020-09-22 06:50:53 +0000

    途中７-users-table-column追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,59 @@
 class BooksController < ApplicationController
   
   def index
-    
+    # @books = Book.all
+    # @book = Book.new
   end
   
   def show
     @book = Book.find(params[:id])
   end
+  
+  def edit
+      @book = Book.find(book_params[:id]) 
+    # if @book.save
+    #   flash[:notice] = "You have updated book successfully."
+    #   redirect_to book_path(@book.id)
+    # else
+    #   @books = Book.all
+    #   render :index
+    # end
+  end
+  
+  def update
+      @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully.."
+     redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+     render :edit
+    end
+  end
+  
+  def delete
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+  
+  def create
+      @book = Book.new(book_params[:id])   
+      # ここがエラー
+    if @book.save
+      flash[:notice] = "You have creatad book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :opinion)
+  end
+  
+  
   
 end
