43d914f7932398b69bd0e3acb598f77c9408adc8
EC2 Default User <ec2-user@ip-172-31-3-81.ec2.internal>
2020-09-22 15:06:21 +0000

    途中８

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def about
+  end
+
+
+  # private
+  # def login_check
+  #   unless user_signed_in?
+  #     # flash[:alert] = "ログインしてください"
+  #     redirect_to root_path
+  #   end
+  # end
 end
